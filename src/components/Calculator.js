1fb43105b9c441a84b7dd70f0151fe31fd3cf18a
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-17 22:56:00 +0100

    update required changes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,28 @@
-function Calculator() {
-  return (
-    <div className="calculatorContainer">
-      <div className="box display"><span className="displayNumber">0</span></div>
-      <div className="box"><button type="button">AC</button></div>
-      <div className="box"><button type="button">+/-</button></div>
-      <div className="box"><button type="button">%</button></div>
-      <div className="box"><button className="orange" type="button">÷</button></div>
-      <div className="box"><button type="button">7</button></div>
-      <div className="box "><button type="button">8</button></div>
-      <div className="box"><button type="button">9</button></div>
-      <div className="box"><button className="orange" type="button">x</button></div>
-      <div className="box"><button type="button">4</button></div>
-      <div className="box"><button type="button">5</button></div>
-      <div className="box"><button type="button">6</button></div>
-      <div className="box"><button className="orange" type="button">-</button></div>
-      <div className="box"><button type="button">1</button></div>
-      <div className="box"><button type="button">2</button></div>
-      <div className="box"><button type="button">3</button></div>
-      <div className="box "><button type="button" className="orange">+</button></div>
-      <div className="box zero"><button type="button" className="thin">0</button></div>
-      <div className="box"><button type="button" className="thin">.</button></div>
-      <div className="box"><button type="button" className="orange thin">=</button></div>
-    </div>
-  );
-}
+import Display from './Display';
+
+const Calculator = () => (
+  <div className="calculatorContainer">
+    <Display />
+    <div className="box"><button type="button">AC</button></div>
+    <div className="box"><button type="button">+/-</button></div>
+    <div className="box"><button type="button">%</button></div>
+    <div className="box"><button className="orange" type="button">÷</button></div>
+    <div className="box"><button type="button">7</button></div>
+    <div className="box "><button type="button">8</button></div>
+    <div className="box"><button type="button">9</button></div>
+    <div className="box"><button className="orange" type="button">x</button></div>
+    <div className="box"><button type="button">4</button></div>
+    <div className="box"><button type="button">5</button></div>
+    <div className="box"><button type="button">6</button></div>
+    <div className="box"><button className="orange" type="button">-</button></div>
+    <div className="box"><button type="button">1</button></div>
+    <div className="box"><button type="button">2</button></div>
+    <div className="box"><button type="button">3</button></div>
+    <div className="box "><button type="button" className="orange">+</button></div>
+    <div className="box zero"><button type="button" className="thin">0</button></div>
+    <div className="box"><button type="button" className="thin">.</button></div>
+    <div className="box"><button type="button" className="orange thin">=</button></div>
+  </div>
+);
 
 export default Calculator;
