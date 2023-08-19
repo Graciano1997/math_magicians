aeedd0b1a361aa3f6a5714b652437020a8d51386
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-19 08:18:15 +0100

    add fetch method

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,49 @@
-const Quotes = () => (
-  <div className="quoteContainer">
-    <h1>I am the quote component</h1>
-  </div>
-);
+import { useState, useEffect } from 'react';
+
+const Quotes = () => {
+  const [quote, setQuote] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => () => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes', {
+          headers: { 'X-Api-Key': 'CmTKM6B4Q5xkeSVvEc3FObE4vXWoMUq0yCAe0L0D' },
+        });
+        const quoteArray = await res.json();
+        const random = quoteArray[0];
+        setQuote(random);
+        setIsLoading(false);
+      } catch (error) {
+        setHasError(true);
+        setIsLoading(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (hasError) {
+    return (
+      <div className="quoteContainer">
+        <p>There is an Error</p>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="quoteContainer">
+        <p>Loading ...</p>
+      </div>
+    );
+  }
+  return (
+    <div className="quoteContainer">
+      {quote && (<blockquote>{quote.quote}</blockquote>)}
+      {quote && (<p>{`Author: ${quote.author} `}</p>)}
+    </div>
+  );
+};
 
 export default Quotes;
